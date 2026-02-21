797e9361a5631c3eb49e4b83160ed2225a1aead1
Yago Ibarrola <yago.ibarrolal@um.es>
2026-02-21 14:03:42 +0100

    Taxi SARSA -SARSA -Expected SARSA -NstepSARSA file created

--- Entornos_Complejos/taxi_gif.py
+++ Entornos_Complejos/taxi_gif.py
@@ -1,5 +1,6 @@
 import gymnasium as gym
 import imageio
+import numpy as np
 from IPython.display import Image, display
 
 def animar_estados_taxi_gif(secuencia_estados, nombre_archivo="secuencia_taxi.gif", fps=4):
@@ -20,6 +21,11 @@ def animar_estados_taxi_gif(secuencia_estados, nombre_archivo="secuencia_taxi.gi
         
     # Cerramos el entorno para liberar los recursos
     env.close()
+
+    # Añadimos un par de fotogramas negros al final para marcar el fin de la secuencia
+    
+    fotogramas.append(np.zeros_like(fotogramas[0]))
+    fotogramas.append(np.zeros_like(fotogramas[0]))
 
     # Generamos el archivo GIF uniendo todos los fotogramas
     imageio.mimsave(nombre_archivo, fotogramas, fps=fps, loop=0)
